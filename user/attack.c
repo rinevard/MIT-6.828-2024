a4fbb72da1dc3a7189213edd655201a65a6ece65
rinevard <rinevard@outlook.com>
2025-06-03 20:25:30 +0800

    format code

--- user/attack.c
+++ user/attack.c
@@ -3,11 +3,9 @@
 #include "user/user.h"
 #include "kernel/riscv.h"
 
-int
-main(int argc, char *argv[])
-{
-  // your code here.  you should write the secret to fd 2 using write
-  // (e.g., write(2, secret, 8)
+int main(int argc, char *argv[]) {
+    // your code here.  you should write the secret to fd 2 using write
+    // (e.g., write(2, secret, 8)
 
-  exit(1);
+    exit(1);
 }
--- user/secret.c
+++ user/secret.c
@@ -3,18 +3,14 @@
 #include "user/user.h"
 #include "kernel/riscv.h"
 
-
-int
-main(int argc, char *argv[])
-{
-  if(argc != 2){
-    printf("Usage: secret the-secret\n");
-    exit(1);
-  }
-  char *end = sbrk(PGSIZE*32);
-  end = end + 9 * PGSIZE;
-  strcpy(end, "my very very very secret pw is:   ");
-  strcpy(end+32, argv[1]);
-  exit(0);
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Usage: secret the-secret\n");
+        exit(1);
+    }
+    char *end = sbrk(PGSIZE * 32);
+    end = end + 9 * PGSIZE;
+    strcpy(end, "my very very very secret pw is:   ");
+    strcpy(end + 32, argv[1]);
+    exit(0);
 }
-
