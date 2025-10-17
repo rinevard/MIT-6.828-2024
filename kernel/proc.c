4b37c0935d7393b34520db00fefc3aaaeddf5490
Rinevard <rinevard@outlook.com>
2025-10-17 21:00:22 +0800

    fix some static error

--- kernel/proc.c
+++ kernel/proc.c
@@ -616,8 +616,8 @@ int either_copyin(void *dst, int user_src, uint64 src, uint64 len) {
 // No lock to avoid wedging a stuck machine further.
 void procdump(void) {
     static char *states[] = {
-        [UNUSED] "unused",   [USED] "used",      [SLEEPING] "sleep ",
-        [RUNNABLE] "runble", [RUNNING] "run   ", [ZOMBIE] "zombie"};
+        [UNUSED] = "unused",   [USED] = "used",      [SLEEPING] = "sleep ",
+        [RUNNABLE] = "runble", [RUNNING] = "run   ", [ZOMBIE] = "zombie"};
     struct proc *p;
     char *state;
 
